c611c342438e7ff3b638bd022aaa66554d53a2af
Rony Braswell <ronybraswell@hotmail.com>
2021-04-26 20:49:31 -0400

    Adding working files

---
+++ index.js
@@ -0,0 +1,14 @@
+//create the variables
+const inquirer = require("inquirer");
+const fs = require("fs");
+//need function to take user info
+function writeFile(fileName, data) {
+//need to write file
+function init() {
+//need to enter prompted questions here. use inquirer.prompt
+      console.log(fileName);
+//need to store user input data  
+//notify user
+//write info to file
+//  call to initialize program
+init()
