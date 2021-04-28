65bc62b0cbd93be7514f79688472c1477f3b6155
Rony Braswell <ronybraswell@hotmail.com>
2021-04-27 21:32:03 -0400

    Updating JavaScript files

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-//create the variables
+//create the variable and introduce Inquirer
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+//create variable introduce createMarkdown.js file
+const createMarkdown = require("createMarkdown");
+
 //need function to take user info
 function writeFile(fileName, data) {
-//need to write file
+
+//initalize program
 function init() {
+
+//questions for command line prompts
+    inquirer.prompt([
+        {
+            //enter project title
+            type: "input",
+            name: "title",
+            message: "What is the name of your project?"
+        },
+        {
+            //enter project description
+            type: "input",
+            name: "description",
+            message: "Describe your project"
+        },
+        {
+            //enter the installation instructions
+            type: "input",
+            name: "installation",
+            message: "What are the Projects Installation Instructions?"
+        },
+        {
+            //enter usage information
+            type: "input",
+            name: "usage",
+            message: "Enter Usage Information"
+        },
+        {
+            //enter contribution guidelines
+            type: "input",
+            name: "contribution",
+            message: "Enter the project contribution guidelines"
+        },
+        {
+            //enter test instructions
+            type: "input",
+            name: "tests",
+            message: "Enter the test instructions"
+        },
+    ])    
+
 //need to enter prompted questions here. use inquirer.prompt
       console.log(fileName);
 //need to store user input data  
 //notify user
 //write info to file
 //  call to initialize program
-init()
+init()}}
