738f338bd3df58b24d8ca3c2d9108bf443e60a21
Rony Braswell <ronybraswell@hotmail.com>
2021-05-01 23:01:42 -0400

    Updating working files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
-// creates markdown for read me
-function createMarkdown(data) {
-
-    // Create a function to generate markdown for README
-    return `# ${data.title}
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+return `# ${data.title}
   ----
   ## Table of Contents
   - [Description](#description)
